4a44e93bac745e61cf4bdb94f5f97bda4443fa87
Anna <chmsv.code@gmail.com>
2022-02-20 00:13:48 +0300

    add cors & cookie-parser

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 import express from "express"
+import cors from 'cors'
+import cookieParser from "cookie-parser"
 import { resolve } from "path"
 
 import { Html } from '../client/html.js'
 
 const server = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const __dirname = process.cwd()
 
 const middleware = [
-  express.static(resolve(__dirname, 'dist'))
-]
+  cors(),
+  cookieParser(),
+  express.json({ limit: '50kb' }),
+  express.static(resolve(__dirname, 'dist')),
+];
 
 middleware.forEach((it) => server.use(it))
 
 server.get('/', (req, res) => {
-  res.send('Express server')
+  res.send('Express server');
 })
 
 server.get('/*', (req, res) => {
